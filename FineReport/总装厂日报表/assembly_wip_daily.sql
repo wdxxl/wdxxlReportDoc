4d19025b0e819772220c14d8e00fe0fe2a8e093c
king.wang <wdxxlanswer@gmail.com>
2014-10-28 13:41:11 +0800

    update parameter use 'mtcode' instead of 'mttext3' and 'mttext4';

--- FineReport/总装厂日报表/assembly_wip_daily.sql
+++ FineReport/总装厂日报表/assembly_wip_daily.sql
@@ -1,49 +1,47 @@
 function assembly_wip_daily(i_plcode in varchar2,
-                                                   i_stcode_t00 in varchar2,
-                                                   i_stcode_f23 in varchar2,
-                                                   i_mttext3 in varchar2,
-                                                   i_mttext4 in varchar2,
-                                                   i_crtime in date ) return number
+                            i_stcode_t00 in varchar2,
+                            i_stcode_f23 in varchar2,
+                            i_mtcode in varchar2,
+                            i_crtime in date ) return number
 is
   o_sum_moqty_t00 varchar2 (22);
   o_sum_moqty_f23 varchar2 (22);
 /*
  * *********************************************
- * Name: assembly_wip_daily(i_plcode,i_stcode_t00,i_stcode_f23,i_mttext3,i_mttext4,i_crtime)
+ * Name: assembly_wip_daily(i_plcode,i_stcode_t00,i_stcode_f23,i_mtcode,i_crtime)
  * Description: get assembly online or offline daily records. (i_crtime)
  * Parameter:
 			 i_plcode ZZ0101
 			 i_stcode_t00 T00
-       i_stcode_f23 F23
-			 i_mttext3 KC-1
-			 i_mttext4 KC1
+       	     i_stcode_f23 F23
+			 i_mtcode 'Z99KC135LET9NW0K10'
 			 i_crtime 2014-10-27 00:00:00
+			 
+ ******************************************************************************************			 
  * Verions   date  eidtor  description
  1.0.0    2014-10-27  kexue  create this function
-
- *********************************************
+ 1.0.1	  2014-10-28  kexue  update parameter use 'mtcode' instead of 'mttext3' and 'mttext4';
+ ******************************************************************************************
  *
+ * Test SQL:
+ * select assembly_wip_daily('ZZ0101','T00','F23','Z99KC135LET9NW0K10',format_crtime('2014-08-15 06:00:00')) from dual;
  **/
 begin
   --T00
-   select
+  select
     sum(vc.MOQTY) into o_sum_moqty_t00
   from pp_vehconf vc
     inner join pp_manuorder mo on vc.monum = mo.monum and mo.plcode = i_plcode
-       inner join ba_material mt on mt.mtcode = mo.mtcode
    	where vc.stcode = i_stcode_t00
-   		and mt.mttext3 = i_mttext3
-   		and mt.mttext4 = i_mttext4
+   		and mo.mtcode = i_mtcode
    		and format_crtime(vc.crtime) <= i_crtime;
    --F23
-    select
+  select
     sum(vc.MOQTY) into o_sum_moqty_f23
   from pp_vehconf vc
     inner join pp_manuorder mo on vc.monum = mo.monum and mo.plcode = i_plcode
-       inner join ba_material mt on mt.mtcode = mo.mtcode
    	where vc.stcode = i_stcode_f23
-   		and mt.mttext3 = i_mttext3
-   		and mt.mttext4 = i_mttext4
+   		and mo.mtcode = i_mtcode
    		and format_crtime(vc.crtime) <= i_crtime;
 
   return to_number(nvl(o_sum_moqty_t00,'0') - nvl(o_sum_moqty_f23,'0'));
