f40cf44310bc643ca0e0801cc886cecf48535af0
king.wang <wdxxlanswer@gmail.com>
2014-10-28 13:42:40 +0800

    update parameter use 'mtcode' instead of 'mttext3' and 'mttext4'

--- FineReport/总装厂日报表/assembly_onoffline_daily.sql
+++ FineReport/总装厂日报表/assembly_onoffline_daily.sql
@@ -1,35 +1,34 @@
 function assembly_onoffline_daily(i_plcode in varchar2, 
-                                                   i_stcode in varchar2,
-                                                   i_mttext3 in varchar2,
-                                                   i_mttext4 in varchar2,
-                                                   i_crtime in date) return number
+                                  i_stcode in varchar2,
+                                  i_mtcode in varchar2,
+                                  i_crtime in date) return number
 is
   o_sum_moqty varchar2(22);
 /*
  * *********************************************
- * Name: assembly_onoffline_daily(i_plcode,i_stcode,i_mttext3,i_mttext4,i_crtime)
+ * Name: assembly_onoffline_daily(i_plcode,i_stcode,i_mtcode,i_crtime)
  * Description: get assembly online or offline daily records. (i_crtime)
  * Parameter:
 			 i_plcode ZZ0101
 			 i_stcode T00 or F23
-			 i_mttext3 KC-1
-			 i_mttext4 KC1
+			 i_mtcode 'Z99KC135LET9NW0K10'
 			 i_crtime 2014-10-27 00:00:00
+ ******************************************************************************************
  * Verions   date  eidtor  description
  1.0.0    2014-10-27  kexue  create this function
- 
- *********************************************
+ 1.0.1	  2014-10-28  kexue  update parameter use 'mtcode' instead of 'mttext3' and 'mttext4';
+ ******************************************************************************************
  *
+ * Test SQL:
+ * select assembly_onoffline_daily('ZZ0101','T00','Z99KC135LET9NW0K10',format_crtime('2014-08-15 06:00:00')) from dual;
  **/
 begin
   select 
     sum(vc.MOQTY) into o_sum_moqty
   from pp_vehconf vc
     inner join pp_manuorder mo on vc.monum = mo.monum and mo.plcode = i_plcode
-       inner join ba_material mt on mt.mtcode = mo.mtcode
    	where vc.stcode = i_stcode
-   		and mt.mttext3 = i_mttext3
-   		and mt.mttext4 = i_mttext4
+   		and mo.mtcode = i_mtcode
    		and format_crtime(vc.crtime) = i_crtime;
   return to_number(NVL(o_sum_moqty,'0'));
 end assembly_onoffline_daily;
