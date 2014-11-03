095ab92076358d2269d2082481476e36af6d5471
king.wang <wdxxlanswer@gmail.com>
2014-11-03 17:46:27 +0800

    only get the repaired records;

--- FineReport/FTT报表/ftt2_repair_ok.sql
+++ FineReport/FTT报表/ftt2_repair_ok.sql
@@ -1,9 +1,7 @@
 function ftt2_repair_ok(i_objcode in varchar2,
 						i_productiondate in varchar2) return number
 is
-  o_sum_qstnnum number;
   v_ok_qstnnum varchar2(22);
-  v_all_qstnnum varchar2(22);
 /*
  * *********************************************
  * Name: ftt2_repair_ok(i_objcode,i_productiondate)
@@ -17,6 +15,7 @@ is
  1.0.0    2014-11-03  kexue  create this function;
  1.0.1    2014-11-03  kexue  use distinct count VIN as the repaired car numbers;
  1.0.2    2014-11-03  kexue  get rate for qstnnum of 2 in qstnnum with all record;
+ 1.0.3    2014-11-03  kexue  only get the repaired records;
  ******************************************************************************************
  *
  * Test SQL:
@@ -31,20 +30,5 @@ begin
 		 and to_char(fttqi.crttime,'yyyy-mm-dd') = i_productiondate
 		 and fttqi.brchstatncode = i_objcode;
 
-	select
-		sum(fttqi.qstnnum) into v_all_qstnnum
-	from 
-		 ftt_qualityinfo fttqi 
-	where to_char(fttqi.crttime,'yyyy-mm-dd') = i_productiondate
-		 and fttqi.brchstatncode = i_objcode;
-	
-	  IF nvl(v_all_qstnnum,0) = 0 THEN
-	      o_sum_qstnnum:= 0;
-	  ELSIF nvl(v_ok_qstnnum,0) = 0 THEN
-	      o_sum_qstnnum:= 0;
-	  ELSE 
-	  	o_sum_qstnnum:= round(to_number(v_ok_qstnnum)/to_number(v_all_qstnnum), 4);
-	  End IF;
-
-  return o_sum_qstnnum;
+  return  to_number(nvl(v_ok_qstnnum,0));
 end ftt2_repair_ok;
