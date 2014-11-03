5570dead21097c2ddf80472e3712c1bec8834b1a
king.wang <wdxxlanswer@gmail.com>
2014-11-03 10:13:49 +0800

    add INCLUDEFLOATPOINT (inner join ftt_qualityline table)

--- FineReport/FTT报表/pro_rpt_ftt1.sql
+++ FineReport/FTT报表/pro_rpt_ftt1.sql
@@ -15,6 +15,7 @@
  * Verions   date  eidtor  description
  1.0.0    2014-11-03  kexue create this procedure
  1.0.1    2014-11-03  kexue update FTTvalue for display, formated as '0.1234' rather than '12.34'
+ 1.0.2 	  2014-11-03  kexue add INCLUDEFLOATPOINT (inner join ftt_qualityline table)
  ******************************************************************************************
  *
  * Test SQL:
@@ -29,18 +30,20 @@
 begin
    delete from RPT_FTT1;
    INSERT INTO RPT_FTT1
-   (SITECODE, PRODUCTIONDATE, FTTVALUE)
+   (SITECODE, PRODUCTIONDATE, FTTVALUE, INCLUDEFLOATPOINT)
   select 
     '1081',
     to_date(productiondate, 'yyyy-mm-dd hh24:mi:ss') productiondate,
-    decode(b,0,'0',round(a/b,4)) as FTTValue
+    decode(b,0,'0',round(a/b,4)) as FTTValue,
+    FLOATPOINTFLG
   from
   (select
-    to_char(crttime,'yyyy-mm-dd') productiondate,
-    sum(QSTNNUM) as a,
-    ftt_ok_daily(to_date(to_char(crttime,'yyyy-mm-dd')||' 00:00:00', 'yyyy-mm-dd hh24:mi:ss')) as b
-  from ftt_qualityinfo 
-  group by to_char(crttime,'yyyy-mm-dd'));
+    to_char(fttqi.crttime,'yyyy-mm-dd') productiondate,
+    fttql.FLOATPOINTFLG,
+    sum(fttqi.QSTNNUM) as a,
+    ftt_ok_daily(to_date(to_char(fttqi.crttime,'yyyy-mm-dd')||' 00:00:00', 'yyyy-mm-dd hh24:mi:ss')) as b
+  from ftt_qualityinfo fttqi inner join ftt_qualityline fttql on fttqi.COLTSTATCODE = fttql.STATIONCODE
+  group by to_char(fttqi.crttime,'yyyy-mm-dd'),FLOATPOINTFLG);
 
    commit;
 EXCEPTION
