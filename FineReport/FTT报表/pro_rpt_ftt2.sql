7bda77a10334d730e8997982fc1ecf48e54529a5
king.wang <wdxxlanswer@gmail.com>
2014-11-03 15:09:49 +0800

    FTT2 报表数据完成

---
+++ FineReport/FTT报表/pro_rpt_ftt2.sql
@@ -0,0 +1,58 @@
+PROCEDURE pro_rpt_ftt2
+(RunDate IN date,OUTERRCODE OUT VARCHAR2,OUTERRMSG OUT VARCHAR2) IS
+  var2ErrCode varchar2(256);
+  var2ErrMsg  varchar2(256);
+/*
+ * *********************************************
+ * Name: pro_rpt_ftt2(RunDate,OUTERRCODE,OUTERRMSG)
+ * Description: build FTT2 data
+ * Parameter:
+       RunDate
+       OUTERRCODE
+       OUTERRMSG
+       
+ ******************************************************************************************     
+ * Verions   date  eidtor  description
+ 1.0.0    2014-11-03  kexue create this procedure
+ ******************************************************************************************
+ *
+ * Test SQL:
+ * declare
+	aa varchar2(255);
+	bb varchar2(255);
+   begin
+  	pro_rpt_ftt2(sysdate,aa,bb);
+   end;
+ */
+
+begin
+   delete from RPT_FTT2;
+   INSERT INTO RPT_FTT2
+	(SITECODE, PRODUCTIONDATE, BRCHSTATNCODE, BRCHSTATNNAME, FTTVALUE, CLOSEDRATE, CARS)
+	select 
+		'1081',
+		to_date(productiondate,'yyyy-mm-dd hh24:mi:ss'),
+		objcode,
+		objname, 
+		decode(total_OK_in,0,'0',round(total_issue/total_OK_in, 4)) as FTTValue,
+		decode(total_OK_in,0,'0',round(ftt2_repair_ok(objcode,productiondate)/total_OK_in, 4)) as CLOSEDRATE,
+		total_OK_in
+	from (
+	select 
+		distinct 
+		to_char(fttqi.crttime,'yyyy-mm-dd') productiondate,
+	  	fttpbd.objcode,
+		fttpbd.objname,
+	    sum(fttqi.QSTNNUM) as total_issue,
+	    ftt_ok_daily(to_date(to_char(fttqi.crttime, 'yyyy-mm-dd')||' 00:00:00' , 'yyyy-mm-dd hh24:mi:ss')) as total_OK_in
+	from ftt_qualityinfo fttqi inner join ftt_prdbasedata fttpbd on fttpbd.objcode  = fttqi.brchstatncode
+		group by to_char(fttqi.crttime,'yyyy-mm-dd'),objcode,objname
+	);
+
+   commit;
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    OutErrCode := var2ErrCode;
+    OutErrMsg  := SUBSTR(var2ErrMsg, 1, 255);
+END;
