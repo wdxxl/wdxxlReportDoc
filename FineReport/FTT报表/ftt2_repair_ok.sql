f9c9005da6e35133c0ad061f28d974c91a524923
king.wang <wdxxlanswer@gmail.com>
2014-11-03 15:49:19 +0800

    use distinct count VIN as the repaired car numbers;

--- FineReport/FTT报表/ftt2_repair_ok.sql
+++ FineReport/FTT报表/ftt2_repair_ok.sql
@@ -13,18 +13,21 @@ is
  ******************************************************************************************			 
  * Verions   date  eidtor  description
  1.0.0    2014-11-03  kexue  create this function;
+ 1.0.1    2014-11-03  kexue  use distinct count VIN as the repaired car numbers;
  ******************************************************************************************
  *
  * Test SQL:
  * select ftt2_repair_ok('108104','2014-09-19') from dual;
  **/
 begin
-  select
-	sum(fttqi.QSTNNUM) into o_sum_qstnnum
-  from 
-  	ftt_qualityinfo fttqi 
-  where fttqi.repairstatuscode = 2 
-  	and to_char(fttqi.crttime,'yyyy-mm-dd') = i_productiondate
-  	and fttqi.brchstatncode = i_objcode;
+  select count(vin) into o_sum_qstnnum
+  from
+	  (select
+		distinct fttqi.vin
+	  from 
+	  	ftt_qualityinfo fttqi 
+	  where fttqi.repairstatuscode = 2 
+	  	and to_char(fttqi.crttime,'yyyy-mm-dd') = i_productiondate
+	  	and fttqi.brchstatncode = i_objcode);
   return to_number(nvl(o_sum_qstnnum,'0'));
 end ftt2_repair_ok;
