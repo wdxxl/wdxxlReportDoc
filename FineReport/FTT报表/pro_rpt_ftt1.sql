a6da72e8aa0ef3fea92262b83edd96565595d8f4
king.wang <wdxxlanswer@gmail.com>
2014-11-03 16:06:03 +0800

    Change FTTvalue = sum(distinct vin)/ok offline

--- FineReport/FTT报表/pro_rpt_ftt1.sql
+++ FineReport/FTT报表/pro_rpt_ftt1.sql
@@ -16,6 +16,7 @@
  1.0.0    2014-11-03  kexue create this procedure
  1.0.1    2014-11-03  kexue update FTTvalue for display, formated as '0.1234' rather than '12.34'
  1.0.2 	  2014-11-03  kexue add INCLUDEFLOATPOINT (inner join ftt_qualityline table)
+ 1.0.3	  2014-11-03  kexue Change FTTvalue = sum(distinct vin)/ok offline 
  ******************************************************************************************
  *
  * Test SQL:
@@ -37,13 +38,16 @@ begin
     decode(b,0,'0',round(a/b,4)) as FTTValue,
     FLOATPOINTFLG
   from
-  (select
-    to_char(fttqi.crttime,'yyyy-mm-dd') productiondate,
-    fttql.FLOATPOINTFLG,
-    sum(fttqi.QSTNNUM) as a,
-    ftt_ok_daily(to_date(to_char(fttqi.crttime,'yyyy-mm-dd')||' 00:00:00', 'yyyy-mm-dd hh24:mi:ss')) as b
-  from ftt_qualityinfo fttqi inner join ftt_qualityline fttql on fttqi.COLTSTATCODE = fttql.STATIONCODE
-  group by to_char(fttqi.crttime,'yyyy-mm-dd'),FLOATPOINTFLG);
+  ( select productiondate,FLOATPOINTFLG,b,count(vin) as a
+	 from (select
+	 	distinct
+	    to_char(fttqi.crttime,'yyyy-mm-dd') productiondate,
+	    fttql.FLOATPOINTFLG,
+	    fttqi.vin,
+	    ftt_ok_daily(to_date(to_char(fttqi.crttime,'yyyy-mm-dd')||' 00:00:00', 'yyyy-mm-dd hh24:mi:ss')) as b
+	  from ftt_qualityinfo fttqi inner join ftt_qualityline fttql on fttqi.COLTSTATCODE = fttql.STATIONCODE
+	  ) 
+	  group by productiondate,FLOATPOINTFLG,b);
 
    commit;
 EXCEPTION
